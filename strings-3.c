a22be6c05056322e6db6308c71ece0ff96bfed6d
Joel Okorie <joelokorie1@gmail.com>
2023-11-06 12:09:37 +0000

    my c practice

---
+++ strings-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char sample[] = "From whence cometh my help?\n";
+    char *ps;
+
+    ps = sample;        /* initialize the pointer */
+
+    while(*ps)
+    {
+        putchar(*ps);
+        ps++;
+    }
+    return(0);
+}
