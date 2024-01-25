cd2c8b54d635dda5acab6440f02d249b5c3a0d35
Joel Okorie <joelokorie1@gmail.com>
2024-01-25 02:41:19 +0000

    my practice

---
+++ ctime-6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    time_t tictoc;
+    struct tm *today;
+
+    time(&tictoc);
+    today = localtime(&tictoc);
+    printf("It's now %d:%02d:%02d\n",
+            today->tm_hour,
+            today->tm_min,
+            today->tm_sec);
+    return(0);
+}
---
+++ ctime-8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    time_t tictoc;
+    struct tm *today;
+    char *weekday[] = {
+        "Sunday", "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday"
+    };
+
+    time(&tictoc);
+    today = localtime(&tictoc);
+    printf("Today is %s\n",weekday[today->tm_wday]);
+    return(0);
+}
